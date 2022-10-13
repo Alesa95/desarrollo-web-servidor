e72765f1e29305ee7378f11a0cf52d6a45f71beb
Alejandra <alejandra_santiago_95@hotmail.com>
2022-10-13 13:19:38 +0200

    Revisión de código

--- 02_formularios/index_bueno.php
+++ 02_formularios/index_bueno.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8"></meta>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <!--<link rel="stylesheet" href="styles.css">-->
     <title>Formularios</title>
 </head>
 <body>
