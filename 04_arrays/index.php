69d63d7283ffe1ff2ded4703d88f44ab9f3540f3
Alejandra <alejandra_santiago_95@hotmail.com>
2022-10-27 09:28:16 +0200

    Actualización arrays

--- 04_arrays/index.php
+++ 04_arrays/index.php
@@ -8,43 +8,83 @@
 </head>
 <body>
     <?php
-        $videojuegos = array(
-            "juego1" => "Cyberpunk 2077",
-            "juego2" => "Minecraft",
-            "juego3" => "My Little Ponny"
-        );
-        print_r($videojuegos);
-
-        echo "<br><br>";
-
-        $consolas = [
-            "consola1" => "PS4",
-            "consola2" => "PS5",
-            "consola3" => "Nintendo Switch"
-        ];
-        print_r($consolas);
-
-        echo "<br><br>";
-
-        $personas = [
-            "23242627F" => "Albertiño",
-            "95542627T" => "Armando",
-            "87234455G" => "Pepe"
-        ];
-        echo $personas["23242627G"];
-
-        echo "<br><br>";
-
+        
         $series = [
             'El juego del calamar',
             'La casa de papel',
             'Alice in borderland',
             'The Witcher'
         ];    
-    
-        print_r($series);
+        //  Inserta elementos al final del array
+        array_push($series, "Cuéntame", "Dark");
+
+        $series[] = "Big Bang Theory";
+
+        $series[10] = "La que se avecina";
+
+        array_push($series, "Haikyuu");
+
+        $series[10] = "Aquí no hay quien viva";
+
+        unset($series[10]);
+
+        $series = array_values($series);
+
+        foreach ($series as $indice => $serie) {
+            echo $indice . " => " . $serie . "<br>";
+        }
+
+        /*  MEDIANTE FOR EACH, MOSTRAR TODAS LAS PERSONAS
+            Y SUS DNI EN UNA TABLA
+        */
+        
         echo "<br><br>";
-        echo $series[1];    //  Devuelve 'The Witcher'
+
+        //  Array Personas
+        $personas = [
+            "23242627F" => "Albertiño",
+            "95542627T" => "Armando",
+            "87234455G" => "Pepe"
+        ];
+        
+        $personas["12345678D"] = "Rodolfo";
+
+        $personas["87234455G"] = "Ruperto";
+
+        unset($personas["23242627F"]);
+
+        $personas = array_values($personas);
+
+        print_r($personas);
+
+        echo "<br><br>";
+
+        echo "Hay " . count($personas) . " personas";
     ?>  
+
+
+    <!-- Bucle for para las series -->
+    <ul>
+        <?php
+        for ($i = 0; $i < count($series); $i++) {
+        ?>
+            <li><?php echo $series[$i] ?></li>
+        <?php
+        }
+        ?>
+    </ul>
+
+    <!-- Bucle while para las series -->
+    <ol>
+        <?php 
+        $i = 0;
+        while ($i < count($series)) {
+        ?>
+            <li><?php echo $series[$i] ?></li>
+        <?php
+            $i++;
+        }
+        ?>
+    </ol>
 </body>
 </html>
