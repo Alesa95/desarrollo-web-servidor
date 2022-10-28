dcb20686d80fc52396320dbc2b418b1909d1af0b
Alejandra <alejandra_santiago_95@hotmail.com>
2022-10-28 09:39:01 +0200

    Código mejorado

--- 04_arrays/ejercicios/ejercicio5.php
+++ 04_arrays/ejercicios/ejercicio5.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles.css">
     <title>Ejercicio 5</title>
 </head>
 <body>
     <h1>Ejercicio 5</h1>
 
-    <?php
-
+    <?php   //  FUNCIONES
     function calificacionIf (int $nota) {
         if ($nota < 5) {
             $calificacion = "Suspenso";
@@ -33,15 +33,39 @@
         };
         return $calificacion;
     }
+    ?>
 
-    /*
+    <?php   //  EJERCICIO VERSIÓN 1
     $estudiantes = [
         ["Luis", rand(0,10)],
         ["Alfredo", rand(0,10)],
         ["Elena", rand(0,10)]
     ];
-    */
+    ?>
 
+    <h2>Tabla 1</h2>
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Nota </th>
+            <th>Calificación</th>
+        </tr>
+        <?php
+        foreach ($estudiantes as $estudiante) {
+            list($nombre, $nota) = $estudiante;
+        ?>
+            <tr>
+                <td><?php echo $nombre ?></td>
+                <td><?php echo $nota ?></td>
+                <td><?php echo calificacionMatch($nota) ?>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+
+    <?php   //  EJERCICIO VERSIÓN 2 (CON 3 NOTAS)
     $estudiantes = [
         ["Luis"],
         ["Alfredo"],
@@ -51,26 +75,32 @@
     for ($i = 0; $i < count($estudiantes); $i++) {
         $estudiantes[$i][1] = rand(0,10);
         $estudiantes[$i][2] = rand(0,10);
+        $estudiantes[$i][3] = rand(0,10);
     }
-
     ?>
 
+    <br>
+
+    <h2>Tabla 2</h2>
     <table>
         <tr>
             <th>Nombre</th>
             <th>Nota 1</th>
             <th>Nota 2</th>
-            <th>Califiación</th>
+            <th>Nota 3</th>
+            <th>Calificación media</th>
         </tr>
         <?php
         foreach ($estudiantes as $estudiante) {
-            list($nombre, $nota1, $nota2) = $estudiante;
+            list($nombre, $nota1, $nota2, $nota3) = $estudiante;
+            $media = (int)round(($nota1 + $nota2 + $nota3) / 3);
         ?>
             <tr>
                 <td><?php echo $nombre ?></td>
                 <td><?php echo $nota1 ?></td>
                 <td><?php echo $nota2 ?></td>
-                <td><?php echo calificacionMatch($nota2) ?>
+                <td><?php echo $nota3 ?></td>
+                <td><?php echo calificacionMatch($media) ?>
                 </td>
             </tr>
         <?php
