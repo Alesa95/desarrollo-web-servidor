ca9d2943e36f312e2e4cbb78f3b8d6a2c9d8e2c2
Alejandra <alejandra_santiago_95@hotmail.com>
2022-11-29 09:04:59 +0100

    modificar videojuego

--- 08_laravel/videojuegos/resources/views/videojuegos/show.blade.php
+++ 08_laravel/videojuegos/resources/views/videojuegos/show.blade.php
@@ -14,6 +14,9 @@
         <p>Precio: {{ $videojuego -> precio }}</p>
         <p>PEGI: {{ $videojuego -> pegi }}</p>
         <p>Descripción: {{ $videojuego -> descripcion }}</p>
+        <form method="GET" action="{{ route('videojuegos.edit', ['videojuego' => $videojuego -> id]) }}">
+            <button class="btn btn-primary" type="submit">Editar</button>
+        </form>
     </div> 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
